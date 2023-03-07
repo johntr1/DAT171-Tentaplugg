3fc7d7ea9b3d768ec4c28c3479938c712e558e55
john-tran <johntran1@hotmail.se>
2023-03-07 18:53:02 +0100

    test

--- exam_2022-08/question1.py
+++ exam_2022-08/question1.py
@@ -1,13 +1,40 @@
 # Question 1
 # x[key] = value
+from collections import *
 
 def read_users(file):
     with open(file, mode='r') as file:
         read_file = file.read()
+    li = read_file.split('\n')
+    li_split = [x.split(':') for x in li]
+    x = {}
+    for i, element in enumerate(li_split):
+        test = int(element[2])
+        if test >= 500:
+            names = element[4]
+            key = element[0]
+            x[key] = names
+    return x
 
-    li = read_file.split(':')
-    for i in li:
-        if i ==
-    return li
 
-print(read_users('passwd.txt'))
+def read_groups(file):
+    x = {}
+    with open(file, mode='r') as file:
+        read_file = file.read()
+
+    li = read_file.split('\n')
+    li_split = [x.split(':') for x in li]
+    for i, element in enumerate(names):
+        names = element[3].split(',')
+        for j in enumerate(li_split):
+            return True
+
+
+
+    return x
+
+print(read_groups('group.txt'))
+print(defaultdict)
+
+
+
